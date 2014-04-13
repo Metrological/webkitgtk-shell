b9644da61bf3844fde1a6ff160a75174b7b24a17
Zan Dobersek <zdobersek@igalia.com>
2014-04-13 06:55:32 -0700

    Support loading different Web pages through an env.

--- src/webkitgtk-shell.c
+++ src/webkitgtk-shell.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
 #include <webkit2/webkit2.h>
 
 static gboolean fullscreenWindow(GtkWidget* widget)
@@ -11,11 +12,17 @@ int main(int argc, char* argv[])
 {
     gtk_init(&argc, &argv);
 
+    const char* uri = getenv("WEBKITGTK_SHELL_URI");
+    if (!uri && argc > 1)
+        uri = argv[1];
+    if (!uri)
+        uri = "http://www.webkitgtk.org/";
+
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
     WebKitWebView* webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webkit_web_view_load_uri(webView, argc > 1 ? argv[1] : "http://www.webkitgtk.org/");
+    webkit_web_view_load_uri(webView, uri);
 
     gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webView));
     gtk_widget_show_all(window);
