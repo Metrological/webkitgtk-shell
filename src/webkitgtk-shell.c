34b70e17ac6e3f0b92fee853b1c87e90bee55f39
Zan Dobersek <zdobersek@igalia.com>
2014-05-26 01:04:16 -0700

    Write out console messages to stdout, to help with debugging.

--- src/webkitgtk-shell.c
+++ src/webkitgtk-shell.c
@@ -22,6 +22,10 @@ int main(int argc, char* argv[])
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
     WebKitWebView* webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
+
+    WebKitSettings* settings = webkit_web_view_get_settings(webView);
+    webkit_settings_set_enable_write_console_messages_to_stdout(settings, TRUE);
+
     webkit_web_view_load_uri(webView, uri);
 
     gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webView));
